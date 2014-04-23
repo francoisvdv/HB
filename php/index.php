0337f94266027b37430527699f704a00e3a5104f
Francois van der Ven <francoisvdven@gmail.com>
2014-04-23 17:05:58 +0200

    Added paypal api test credentials

--- php/index.php
+++ php/index.php
@@ -1,5 +1,5 @@
 <?php
 define('PP_CONFIG_PATH', './sdk_config.ini');
 
-$apiContext = new ApiContext(new OAuthTokenCredential('<clientId>', '<clientSecret>'));
+$apiContext = new ApiContext(new OAuthTokenCredential('AXvqnRARhHOqpTw9Elb07_M-x8UvlYaYTEETMqAMmVVDeevrnq19x6S8x4wu', 'EEcrcBAoFMu_UKRjaf67rc0tTlV97738cQgsV9lYkNNfw70LHsz5q0T-1t7o'));
 ?>
